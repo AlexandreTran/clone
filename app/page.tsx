e199ff4552941ed24c11d2029beae8212d509889
AlexandreTran <alex7411@outlook.com>
2024-08-29 21:57:46 +0200

    fdsfds
    fdsfsd

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <Button>Hello from the fsdsusdasx</Button>
+      <Button>Hello from the ux</Button>
     </div>
   );
 }

4a813bbb74a965d3528e924112ee03e9bd5e2c77
AlexandreTran <alex7411@outlook.com>
2024-10-03 21:57:04 +0200

    maj
    maj

--- clone/app/components/ListingCard.tsx
+++ clone/app/components/ListingCard.tsx
@@ -1,7 +1,52 @@
-export function ListingCard() {
+import Image from "next/image";
+import Link from "next/link";
+import { useCountries } from "../lib/getCountries";
+import { AddToFavoriteButton } from "./SubmitButtons";
+
+interface iAppProps {
+  imagePath: string;
+  description: string;
+  location: string;
+  price: number;
+  userId: string | undefined;
+}
+
+export function ListingCard({
+  description,
+  imagePath,
+  location,
+  price,
+  userId,
+}: iAppProps) {
+  const { getCountryByValue } = useCountries();
+  const country = getCountryByValue(location);
   return (
-    <div>
-      <h1>Hello from the Card</h1>
+    <div className="flex flex-col">
+      <div className="relative h-72">
+        <Image
+          src={`https://sxtdsaxqbkrtirqolene.supabase.co/storage/v1/object/public/images/${imagePath}`}
+          alt="Image of the Home"
+          fill
+          className="rounded-lg h-full object-cover mb-3"
+        />
+
+        {userId && (
+          <div className="z-10 absolute top-2 right-2">
+            <AddToFavoriteButton />
+          </div>
+        )}
+      </div>
+      <Link href={"/"} className="mt-2">
+        <h3 className="font-medium text-base">
+          {country?.flag} {country?.label} / {country?.region}
+        </h3>
+        <p className="text-muted-foreground text-sm line-clamp-2">
+          {description}
+        </p>
+        <p className="pt-2 text-muted-foreground">
+          <span className="font-medium text-black">${price}</span>/night
+        </p>
+      </Link>
     </div>
   );
 }
